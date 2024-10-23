0572a87eae23bc7a1f2ef056587d82d6583fb819
izzudien al faqih <sehzadealhasan@gmail.com>
2024-10-23 22:18:26 +0700

    kalkulator

---
+++ kalkulator.js
@@ -0,0 +1,20 @@
+var nilai1 = parseInt(prompt("Masukkan nilai 1"))
+var operator = prompt ("Masukkan operator + - * /")
+var nilai2 = parseInt(prompt("Masukkan nilai 2"))
+
+
+if(operator == '+'){
+    alert(nilai1 + nilai2)
+}
+
+else if (operator == '-'){
+    alert(nilai1 - nilai2)
+}
+
+else if(operator== '*'){
+    alert(nilai1*nilai2)
+}
+
+else if(operator== '/'){
+    alert(nilai1/nilai2)
+}
